e2049274f92c78bc67cb69956af0f54f3655dd0f
Carlos Kassab <carlos.kassab@nextiva.com>
2023-11-11 23:05:55 -0600

    Kinda editing the first apartment dashboard

--- src/components/apartmentManagement/ApartmentCard.tsx
+++ src/components/apartmentManagement/ApartmentCard.tsx
@@ -1,8 +1,8 @@
-import { Box, Card, Divider, Grid, IconButton, styled } from "@mui/material";
-import { H3, H6, Small, Tiny } from "components/Typography";
+import {Box, Card, Divider, Grid, IconButton, styled} from "@mui/material";
+import {H1, H3, H4, H6, Small, Tiny} from "components/Typography";
 import UkoAvatar from "components/UkoAvatar";
-import React, { FC } from "react";
-import {User, UserCardProps} from "../userManagement/UserCard";
+import React, {FC} from "react";
+import {User} from "../userManagement/UserCard";
 
 // component props interface
 export interface ApartmentCardProps {
@@ -17,9 +17,8 @@ export interface Apartment {
 }
 
 
-
 // styled components
-const ImageWrapper = styled(Box)(({ theme }) => ({
+const ImageWrapper = styled(Box)(({theme}) => ({
   height: 100,
   position: "relative",
   "&::before": {
@@ -34,74 +33,50 @@ const ImageWrapper = styled(Box)(({ theme }) => ({
   },
 }));
 
-const StyledAvatar = styled(UkoAvatar)(({ theme }) => ({
-  zIndex: 1,
-  width: 50,
-  height: 50,
-  bottom: -25,
-  position: "absolute",
-  left: "50%",
-  right: "50%",
-  transform: "translateX(-50%)",
-  border: "2px solid",
-  borderColor: theme.palette.background.paper,
-}));
 
-const ApartmentCard: FC<ApartmentCardProps> = ({ apartment }) => {
+
+const ApartmentCard: FC<ApartmentCardProps> = ({apartment}) => {
+  const coverPath = Math.floor(Math.random() * 6) + 1
   return (
-    <Card>
-      <ImageWrapper>
-        <img src="/static/cover/cover-1.png" width="100%" height="100%" alt="{apartment.name}" />
+      <Card>
+        <ImageWrapper>
+          <img src={"/static/cover/cover-" + coverPath + ".png"} width="100%" height="100%"
+               alt="{apartment.name}"/>
+        </ImageWrapper>
 
-        <StyledAvatar src="/static/avatar/001-man.svg" alt="{apartment.name}" />
-      </ImageWrapper>
+        <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              flexDirection: "column",
+              marginTop: 5,
+            }}
+        >
+          <H4 color="text.disabled" fontWeight={500}>
+            Apartamento #:
+          </H4>
+          <H1>{apartment.roomNumber}</H1>
 
-      <Box
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          flexDirection: "column",
-          marginTop: 5,
-        }}
-      >
-        <H6>"apartment"</H6>
-        <Tiny color="text.disabled" fontWeight={500}>
-          Marketing Manager
-        </Tiny>
 
-        <Box marginTop={2}>
-          <IconButton>
-            <img src="/static/social-media/facebook.svg" alt="" />
-          </IconButton>
-          <IconButton>
-            <img src="/static/social-media/twitter.svg" alt="" />
-          </IconButton>
-          <IconButton>
-            <img src="/static/social-media/instagram.svg" alt="" />
-          </IconButton>
-          <IconButton>
-            <img src="/static/social-media/dribble.svg" alt="" />
-          </IconButton>
         </Box>
-      </Box>
 
-      <Divider sx={{ my: 2 }} />
+        {/*<Divider sx={{ my: 2 }} />*/}
 
-      <Grid container spacing={3} mb={2}>
-        <Grid item xs={4} textAlign="center">
-          <H3>{apartment.floor}</H3>
-          <Small color="text.disabled">Post Avg.</Small>
-        </Grid>
-        <Grid item xs={4} textAlign="center">
-          <H3>{apartment.roomNumber}</H3>
-          <Small color="text.disabled">Followers</Small>
-        </Grid>
-        <Grid item xs={4} textAlign="center">
-          <H3>{apartment.user.firstName}</H3>
-          <Small color="text.disabled">Following</Small>
-        </Grid>
-      </Grid>
-    </Card>
+        {/*<Grid container spacing={3} mb={2}>*/}
+        {/*  <Grid item xs={4} textAlign="center">*/}
+        {/*    <H3>{apartment.floor}</H3>*/}
+        {/*    <Small color="text.disabled">Post Avg.</Small>*/}
+        {/*  </Grid>*/}
+        {/*  <Grid item xs={4} textAlign="center">*/}
+        {/*    <H3>{apartment.roomNumber}</H3>*/}
+        {/*    <Small color="text.disabled">Followers</Small>*/}
+        {/*  </Grid>*/}
+        {/*  <Grid item xs={4} textAlign="center">*/}
+        {/*    <H3>{apartment.user.firstName}</H3>*/}
+        {/*    <Small color="text.disabled">Following</Small>*/}
+        {/*  </Grid>*/}
+        {/*</Grid>*/}
+      </Card>
   );
 };
 
